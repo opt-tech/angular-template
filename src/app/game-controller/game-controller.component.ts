0e6bc1055f4ea9d61cc2a94d39764318e092b0fe
GoobyeEarth <ra4chrome.ctrl@gmail>
2018-03-18 11:39:27 +0900

    feet: add onSelect of game-controller

--- src/app/game-controller/game-controller.component.ts
+++ src/app/game-controller/game-controller.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-game-controller',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameControllerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onSelect(handType: string) {
+    console.log(handType);
   }
 
 }
