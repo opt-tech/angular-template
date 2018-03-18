2533c7e04e0b23e9a2e79b672997cb1868cf6002
GoobyeEarth <ra4chrome.ctrl@gmail>
2018-03-18 11:35:35 +0900

    chore(component): add components; game and game-controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { GameControllerComponent } from './game-controller/game-controller.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    GameControllerComponent
   ],
   imports: [
     BrowserModule
